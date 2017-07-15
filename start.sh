e76740e871a8167dbeb34a2846c5e565da1f479e
Yuki Imai <imai.yuki@jp.panasonic.com>
2017-07-15 16:18:47 +0900

    ver1.0.2

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 source ../../devel/setup.bash
 
-gnome-terminal -e "roslaunch turtle_war make_field.launch"
+gnome-terminal -e "/opt/ros/indigo/roslaunch turtle_war make_field.launch"
 
 sleep 5
 
-gnome-terminal -e "roslaunch turtle_war spawn_robot.launch"
+gnome-terminal -e "/opt/ros/indigo/roslaunch turtle_war spawn_robot.launch"
 
 
-gnome-terminal -e "roslaunch turtle_war candy.launch"
+gnome-terminal -e "/opt/ros/indigo/roslaunch turtle_war candy.launch"
 
